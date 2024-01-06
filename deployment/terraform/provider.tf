7d59b6e1c769ce0c3b7d605cdfbf634c6df6fb24
rajkumarqt <154618256+rajkumarqt@users.noreply.github.com>
2024-01-06 15:59:30 +0530

    Update provider.tf

--- deployment/terraform/provider.tf
+++ deployment/terraform/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.31.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  # Configuration options
 }
